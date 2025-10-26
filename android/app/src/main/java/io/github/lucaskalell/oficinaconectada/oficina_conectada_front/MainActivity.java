16bd782b8b8d97d6d34718025c236cad31e84243
lucas kalell <kalell306@gmail.com>
2025-10-25 23:00:14 -0300

    iniciando comit do front

---
+++ android/app/src/main/java/io/github/lucaskalell/oficinaconectada/oficina_conectada_front/MainActivity.java
@@ -0,0 +1,6 @@
+package io.github.lucaskalell.oficinaconectada.oficina_conectada_front;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
